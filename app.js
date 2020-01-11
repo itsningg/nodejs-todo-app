a7c97ef651ae17e02135f2c33aaa47e4a44c8c3d
itsningg <nrm.258456@gmail.com>
2020-01-11 15:09:31 +0700

    Add 'add' feature

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
+const fs = require('fs');
 const yargs = require('yargs');
 const chalk = require('chalk');
 
 yargs.version('1.0.0');
+
+yargs.command({
+    command: 'add',
+    describe:'Add new todo. ',
+    builder:{
+        title:{
+            describe:'Title of todo.',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe:'Body of todo.',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        const allTodos = fs.readFileSync('todo.json');
+        const realAllTodos = JSON.parse(allTodos);
+        console.log(realAllTodos);
+        const newTodo = {
+            title: argv.title,
+            body: argv.body
+        };
+        realAllTodos.push(newTodo);
+        const jsonTodo = JSON.stringify(realAllTodos)
+        fs.writeFileSync('todo.json',jsonTodo);
+        console.log(chalk.green('Added!')+ ' New todo. ');
+    }
+});
 
 yargs.command({
 command: 'list',
